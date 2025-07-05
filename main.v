506649bec577011158cbfc3e86ba213804c04c6a
av-86 <mail@av-86.ru>
2025-07-05 16:05:38 +0300

    Refactor blink module and update main module to control two LEDs; adjust .gitignore and update timestamp in main.done

--- main.v
+++ main.v
@@ -1,16 +1,22 @@
+
+module blink(
+	input wire clk,
+	output wire led
+);
+    reg [27:0] cntr;
+    assign led = cntr > 50000000 ? 1 : 0;
+    always @(posedge clk) begin
+        cntr <= (cntr == 100000000) ? 0 : cntr + 1;
+    end
+endmodule
+
 module main(
 	input wire clk,
-	output reg led
+	output wire led5,
+	output wire led6
 );
-    
-    reg [26:0] cntr;
-    
-    always @(posedge clk) begin
-        cntr <= cntr + 1;
-        if(cntr == 50000000) begin
-            cntr <= 0;
-            led <= ~led;
-        end
-    end
+	
+	blink blink_inst1(.clk(clk), .led(led5));
+	blink blink_inst2(.clk(clk), .led(led6));
 
 endmodule
